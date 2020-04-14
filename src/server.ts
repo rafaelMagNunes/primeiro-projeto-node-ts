c3e91c749ef96a32ce4731bee1a91ef3c29c9ab2
rafaelnunesCodeGalaxy <55004937+rafaelnunesCodeGalaxy@users.noreply.github.com>
2020-04-14 15:27:12 -0300

    add appointment model, add apointment repository, add apointment route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+app.use(express.json());
+app.use(routes);
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+app.listen(3334, () => {
+  console.log('Server started on port 3334!');
 });
